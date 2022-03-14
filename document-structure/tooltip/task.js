e1af3fa11a2e4ece023fe241c17c8282f0b6e44d
Elena Tarasova <tarasova7676@mail.ru>
2022-03-14 15:07:27 +0300

    added hw document-structure

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,31 @@
+const click = (event) => {
+    const target = event.target.closest('.has-tooltip');
+  
+    if (!target) {
+      return;
+    }
+    event.preventDefault();
+  
+    const removeTooltip = document.querySelector('.tooltip');
+  
+    if (removeTooltip) {
+      removeTooltip.remove();
+  
+      if (target.title === removeTooltip.textContent) {
+        return;
+      }
+    }
+  
+    const tooltip = document.createElement('div');
+    tooltip.textContent = target.title;
+    tooltip.className = 'tooltip tooltip_active';
+    tooltip.style.position = 'absolute';
+    document.body.append(tooltip);
+  
+    tooltip.style.top =
+      target.getBoundingClientRect().bottom + window.pageYOffset + 'px';
+    tooltip.style.left = target.getBoundingClientRect().left + 'px';
+  };
+  
+  document.body.addEventListener('click', click);
+  
