0269ed65936691f134ead19ee377196b9f83bb03
Elena Tarasova <tarasova7676@mail.ru>
2022-03-14 14:21:09 +0300

    fixed keysolo

---
+++ event-object/keysolo/dataBase.js
@@ -0,0 +1,17 @@
+const dataBase = [
+    'I love English',
+    'Я люблю кошек',
+    'Я люблю Java Script',
+    'bob',
+    'awesome',
+    'netology',
+    'hello',
+    'kitty',
+    'rock',
+    'youtube',
+    'popcorn',
+    'cinema',
+    'love',
+    'javascript',
+  ];
+  
